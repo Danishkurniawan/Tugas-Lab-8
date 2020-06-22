3d75ef4d37f8d0ae66df7b2f0c34b95eedb165ff
Danish Kurniawan <61901597+Danishkurniawan@users.noreply.github.com>
2020-06-22 11:03:14 +0700

    Tugas Lab 8

---
+++ Karyawan.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugaslab8
+{
+    public abstract class Karyawan
+    {
+        public string NIK { get; set; }
+        public string NAMA { get; set; }
+        public abstract double Gaji();
+    }
+}
